5e3d2abb9903e8b846cc4405ee14630c383b7fb8
Nandhana A Regi <nandhanaaregi@gmail.com>
2023-12-24 00:21:32 +0530

    Refactor for light mode toggling

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function toggleBackground() {
     let body = document.getElementById("container-body");
     let navLinks = document.querySelectorAll(".menu li a");
+    let introSections = document.querySelectorAll(".intro");
+    let containerSections = document.querySelectorAll(".container");
+    let inputButton = document.querySelector(".menu li input");
 
     body.classList.toggle('dark-background');
 
@@ -9,19 +12,38 @@ function toggleBackground() {
         navLinks.forEach(link => {
             link.style.color = 'var(--bgcolor)';
         });
+
+        introSections.forEach(section => {
+            section.style.backgroundColor = '#121212';
+        });
+
+        containerSections.forEach(section => {
+            section.style.backgroundColor = '#0e0e11';
+            let h3 = section.querySelector("h3");
+            if (h3) {
+                h3.style.color = 'white';
+            }
+        });
+        inputButton.style.backgroundColor = 'transparent';
+        inputButton.style.color = 'white';
     } else {
         // Light mode
         navLinks.forEach(link => {
             link.style.color = 'black';
         });
+
+        introSections.forEach(section => {
+            section.style.backgroundColor = 'whitesmoke';
+        });
+
+        containerSections.forEach(section => {
+            section.style.backgroundColor = '#dcdcdc';
+            let h3 = section.querySelector("h3");
+            if (h3) {
+                h3.style.color = 'black';
+            }
+        });
+         inputButton.style.backgroundColor = 'transparent';
+        inputButton.style.color = 'black';
     }
 }
-
-
-// function toggleBackground() {
-//     let body = document.getElementById("container-body");
-//     body.classList.toggle('dark-background');
-// }
-
-// window.addEventListener("hashchange", function() { scrollBy(0, -70) })
-
